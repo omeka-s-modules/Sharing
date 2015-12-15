d80e9ceb61e49334163b6f6f9a0a21e9cd14170a
patrickmj <patrickmjchnm@gmail.com>
2015-12-15 12:34:56 -0500

    first commit`

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+namespace Sharing;
+
+use Omeka\Module\AbstractModule;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\View\Renderer\PhpRenderer;
+use Zend\EventManager\SharedEventManagerInterface;
+
+class Sharing extends AbstractModule
+{
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            'Omeka\Controller\SiteAdmin\IndexController',
+            'site_settings.form',
+            [$this, 'addSiteEnableCheckbox']
+        );
+    }
+    
+    public function addSiteEnableCheckbox()
+    {
+        echo 'what?';
+    }
+}
