5b1d34cea02d5265fc0d8afe540a55f477045e6d
patrickmj <patrickmjchnm@gmail.com>
2016-02-03 10:50:06 -0500

    put in OG data for item page

--- Module.php
+++ Module.php
@@ -16,6 +16,11 @@ class Module extends AbstractModule
         $this->siteSettings = $this->getServiceLocator()->get('Omeka\SiteSettings');
     }
     
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+    
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
         $sharedEventManager->attach(
@@ -25,9 +30,21 @@ class Module extends AbstractModule
         );
         
         $sharedEventManager->attach(
-                array('Omeka\Controller\Site\Item', 'Omeka\Controller\Site\Page'),
+                array('Omeka\Controller\Site\Item',
+                      'Omeka\Controller\Site\Index',
+                      'Omeka\Controller\Site\Page'
+                      ),
                 'view.show.after',
-                array($this, 'insertJavascript')
+                array($this, 'viewShowAfter')
+                );
+        
+        $sharedEventManager->attach(
+                array('Omeka\Controller\Site\Item',
+                      'Omeka\Controller\Site\Index',
+                      'Omeka\Controller\Site\Page'
+                      ),
+                'view.show.after',
+                array($this, 'insertOpenGraphData')
                 );
     }
     
@@ -41,8 +58,28 @@ class Module extends AbstractModule
             'options' => [
                 'label' => $translator->translate('Enable the Sharing module for this site.'),
             ],
-            'attributes' => array('value' => $this->siteSettings->get('sharing_enable'))
+            'attributes' => [
+                'value' => (bool) $this->siteSettings->get('sharing_enable', false)
+            ]
         ]);
+        echo $this->siteSettings->get('sharing_enable');
+    }
+    
+    public function insertOpenGraphData($event)
+    {
+        $view = $event->getTarget();
+        $escape = $view->plugin('escapeHtml');
+        if (isset($view->item)) {
+            $description = $escape($view->item->displayDescription());
+            $view->headMeta()->appendProperty('og:description', $description);
+            if ($primaryMedia = $view->item->primaryMedia()) {
+                $image = $escape($primaryMedia->thumbnailUrl('square'));
+                $view->headMeta()->appendProperty('og:image', $image);
+            }
+        }
+        $view->headMeta()->appendProperty('og:title', $view->headTitle()->renderTitle());
+        $view->headMeta()->appendProperty('og:type', 'website');
+        $view->headMeta()->appendProperty('og:url', $view->serverUrl(true));
     }
     
     public function insertJavascript($event)
@@ -50,5 +87,13 @@ class Module extends AbstractModule
         if ($this->siteSettings->get('sharing_enable')) {
             echo "<script type='text/javascript'> alert('hello'); </script>";
         }
-    } 
+    }
+    
+    public function viewShowAfter($event)
+    {
+        $view = $event->getTarget();
+        $escape = $view->plugin('escapeHtml');
+        $translator = $this->getServiceLocator()->get('MvcTranslator');
+        echo $view->partial('share-buttons', array('escape' => $escape, 'translator' => $translator));
+    }
 }
