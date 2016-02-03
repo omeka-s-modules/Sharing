6ee774d5014f73639af217d6fc1d594a35f2d2df
patrickmj <patrickmjchnm@gmail.com>
2016-02-03 10:50:47 -0500

    set up view stuff

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,8 @@
 <?php
-return array();
-
+return array(
+    'view_manager' => array(
+        'template_path_stack'      => array(
+            OMEKA_PATH . '/modules/Sharing/view',
+        ),
+    ),
+);
