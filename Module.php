a6a16b3d657e845d9513e06d9604a2aa8aaa0c98
patrickmj <patrickmjchnm@gmail.com>
2016-02-05 17:01:35 -0500

    fix check for whether to show buttons

--- Module.php
+++ Module.php
@@ -8,14 +8,6 @@ use Zend\Mvc\MvcEvent;
 
 class Module extends AbstractModule
 {
-    protected $siteSettings;
-    
-    public function onBootstrap(MvcEvent $event)
-    {
-        parent::onBootstrap($event);
-        $this->siteSettings = $this->getServiceLocator()->get('Omeka\SiteSettings');
-    }
-    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
@@ -28,7 +20,7 @@ class Module extends AbstractModule
             'site_settings.form',
             [$this, 'addSiteEnableCheckbox']
         );
-        
+            
         $sharedEventManager->attach(
                 array('Omeka\Controller\Site\Item',
                       'Omeka\Controller\Site\Index',
@@ -50,6 +42,7 @@ class Module extends AbstractModule
     
     public function addSiteEnableCheckbox($event)
     {
+        $siteSettings = $this->getServiceLocator()->get('Omeka\SiteSettings');
         $form = $event->getParam('form');
         $translator = $form->getTranslator();
         $form->add([
@@ -59,102 +52,109 @@ class Module extends AbstractModule
                 'label' => $translator->translate('Enable the Sharing module for this site.'),
             ],
             'attributes' => [
-                'value' => (bool) $this->siteSettings->get('sharing_enable', false)
+                'value' => (bool) $siteSettings->get('sharing_enable', false)
             ]
         ]);
-        echo $this->siteSettings->get('sharing_enable');
     }
     
     public function insertOpenGraphData($event)
     {
-        $routeMatch = $this->getServiceLocator()->get('Application')
-                        ->getMvcEvent()->getRouteMatch();
-        $controller = $routeMatch->getParam('controller');
-        
-        $view = $event->getTarget();
-        $escape = $view->plugin('escapeHtml');
-        switch  ($controller) {
-            case 'Omeka\Controller\Site\Item' :
-                $description = $escape($view->item->displayDescription());
-                $view->headMeta()->appendProperty('og:description', $description);
-                if ($primaryMedia = $view->item->primaryMedia()) {
-                    $image = $escape($primaryMedia->thumbnailUrl('square'));
+        $siteSettings = $this->getServiceLocator()->get('Omeka\SiteSettings');
+        if ($siteSettings->get('sharing_enable')) {
+            $routeMatch = $this->getServiceLocator()->get('Application')
+                            ->getMvcEvent()->getRouteMatch();
+            $controller = $routeMatch->getParam('controller');
+            
+            $view = $event->getTarget();
+            $escape = $view->plugin('escapeHtml');
+            switch  ($controller) {
+                case 'Omeka\Controller\Site\Item' :
+                    $description = $escape($view->item->displayDescription());
+                    $view->headMeta()->appendProperty('og:description', $description);
+                    if ($primaryMedia = $view->item->primaryMedia()) {
+                        $image = $escape($primaryMedia->thumbnailUrl('square'));
+                        $view->headMeta()->appendProperty('og:image', $image);
+                    }
+                break;
+                    
+                case 'Omeka\Controller\Site\Index':
+                    $description = '';
+                    $image = '';
+                    $view->headMeta()->appendProperty('og:description', $description);
                     $view->headMeta()->appendProperty('og:image', $image);
-                }
-            break;
-                
-            case 'Omeka\Controller\Site\Index':
-                $description = '';
-                $image = '';
-                $view->headMeta()->appendProperty('og:description', $description);
-                $view->headMeta()->appendProperty('og:image', $image);
-            break;
-                
-            case 'Omeka\Controller\Site\Page':
-                // need to figure out how to handle the different block types and finding an image
-                $description = '';
-                $block = $view->page->blocks()[0];
-                $attachment = $block->attachments()[0];
-                $item = $attachment->item();
-                if ($primaryMedia = $item->primaryMedia()) {
-                    $image = $escape($primaryMedia->thumbnailUrl('square'));
+                break;
+                    
+                case 'Omeka\Controller\Site\Page':
+                    // need to figure out how to handle the different block types and finding an image
+                    $description = '';
+                    $block = $view->page->blocks()[0];
+                    $attachment = $block->attachments()[0];
+                    $item = $attachment->item();
+                    if ($primaryMedia = $item->primaryMedia()) {
+                        $image = $escape($primaryMedia->thumbnailUrl('square'));
+                        $view->headMeta()->appendProperty('og:image', $image);
+                    }
+                    $image = '';
+                    $view->headMeta()->appendProperty('og:description', $description);
                     $view->headMeta()->appendProperty('og:image', $image);
-                }
-                $image = '';
-                $view->headMeta()->appendProperty('og:description', $description);
-                $view->headMeta()->appendProperty('og:image', $image);
-            break;
+                break;
+            }
+            $view->headMeta()->appendProperty('og:title', $view->headTitle()->renderTitle());
+            $view->headMeta()->appendProperty('og:type', 'website');
+            $view->headMeta()->appendProperty('og:url', $view->serverUrl(true));
         }
-        $view->headMeta()->appendProperty('og:title', $view->headTitle()->renderTitle());
-        $view->headMeta()->appendProperty('og:type', 'website');
-        $view->headMeta()->appendProperty('og:url', $view->serverUrl(true));
     }
     
     public function viewShowAfter($event)
     {
-        $view = $event->getTarget();
-        $view->headScript()->appendFile('https://platform.twitter.com/widgets.js');
-        $view->headLink()->appendStylesheet($view->assetUrl('css/sharing.css', 'Sharing'));
-        $escape = $view->plugin('escapeHtml');
-        $translator = $this->getServiceLocator()->get('MvcTranslator');
-        echo $view->partial('share-buttons', array('escape' => $escape, 'translator' => $translator));
         
-        $fbJavascript = "
-        <script>
-          window.fbAsyncInit = function() {
-            FB.init({
-              xfbml      : true,
-              version    : 'v2.5'
-            });
-          };
-        
-          (function(d, s, id){
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) {return;}
-             js = d.createElement(s); js.id = id;
-             js.src = '//connect.facebook.net/en_US/sdk.js';
-             fjs.parentNode.insertBefore(js, fjs);
-           }(document, 'script', 'facebook-jssdk'));
-        </script>
-        ";
-        echo $fbJavascript;
-        
-        $pinterstJavascript = '
-        
-            <script
-                type="text/javascript"
-                async defer
-                src="//assets.pinterest.com/js/pinit.js"
-            ></script>
-        
-        ';
-        echo $pinterstJavascript;
-        
-        $tumblrJavascript = '
-            <script id="tumblr-js" async src="https://assets.tumblr.com/share-button.js"></script>
-        ';
-        
-        echo $tumblrJavascript;
-        
+        $siteSettings = $this->getServiceLocator()->get('Omeka\SiteSettings');
+        if ($siteSettings->get('sharing_enable')) {
+            $enabledSites = $siteSettings->get('sharing_sites');
+            $view = $event->getTarget();
+            $view->headScript()->appendFile('https://platform.twitter.com/widgets.js');
+            $view->headLink()->appendStylesheet($view->assetUrl('css/sharing.css', 'Sharing'));
+            $escape = $view->plugin('escapeHtml');
+            $translator = $this->getServiceLocator()->get('MvcTranslator');
+            echo $view->partial('share-buttons', array('escape' => $escape, 'translator' => $translator));
+            
+            $fbJavascript = "
+            <script>
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml      : true,
+                  version    : 'v2.5'
+                });
+              };
+            
+              (function(d, s, id){
+                 var js, fjs = d.getElementsByTagName(s)[0];
+                 if (d.getElementById(id)) {return;}
+                 js = d.createElement(s); js.id = id;
+                 js.src = '//connect.facebook.net/en_US/sdk.js';
+                 fjs.parentNode.insertBefore(js, fjs);
+               }(document, 'script', 'facebook-jssdk'));
+            </script>
+            ";
+            
+            $pinterstJavascript = '
+            
+                <script
+                    type="text/javascript"
+                    async defer
+                    src="//assets.pinterest.com/js/pinit.js"
+                ></script>
+            
+            ';
+            
+            $tumblrJavascript = '
+                <script id="tumblr-js" async src="https://assets.tumblr.com/share-button.js"></script>
+            ';
+            
+            foreach($enabledSites as $site) {
+                $js = $site . 'Javascript';
+                echo $js;
+            }
+        }
     }
 }
