73a6f8527d719c80b1ab8add2b240c04c6a5271b
LABOURET Thibaut (i-BP) <thibaut.labouret@i-bp.fr>
2020-05-22 21:46:23 +0200

    feat(*): Browse sharing and Facebook options
    Add sharing for browse item view (item set) and add options for Facebook button.

--- Module.php
+++ Module.php
@@ -12,7 +12,7 @@ class Module extends AbstractModule
 {
     public function getConfig()
     {
-        return include __DIR__.'/config/module.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
 
     public function onBootstrap(MvcEvent $event)
@@ -53,41 +53,59 @@ class Module extends AbstractModule
                 $controller,
                 'view.show.before',
                 [$this, 'viewShow']
-                );
+            );
 
             $sharedEventManager->attach(
                 $controller,
                 'view.show.after',
                 [$this, 'viewShow']
-                );
+            );
         }
 
         $sharedEventManager->attach(
-                'Omeka\Controller\Site\Item',
-                'view.show.after',
-                [$this, 'insertOpenGraphData']
-                );
+            'Omeka\Controller\Site\Item',
+            'view.browse.before',
+            [$this, 'viewBrowse']
+        );
 
         $sharedEventManager->attach(
-                'Omeka\Controller\Site\Index',
-                'view.show.after',
-                [$this, 'insertOpenGraphData']
-                );
+            'Omeka\Controller\Site\Item',
+            'view.browse.after',
+            [$this, 'viewBrowse']
+        );
 
         $sharedEventManager->attach(
-               'Omeka\Controller\Site\Page',
-                'view.show.after',
-                [$this, 'insertOpenGraphData']
-                );
+            'Omeka\Controller\Site\Item',
+            'view.browse.after',
+            [$this, 'insertOpenGraphData']
+        );
+
+        $sharedEventManager->attach(
+            'Omeka\Controller\Site\Item',
+            'view.show.after',
+            [$this, 'insertOpenGraphData']
+        );
+
+        $sharedEventManager->attach(
+            'Omeka\Controller\Site\Index',
+            'view.show.after',
+            [$this, 'insertOpenGraphData']
+        );
+
+        $sharedEventManager->attach(
+            'Omeka\Controller\Site\Page',
+            'view.show.after',
+            [$this, 'insertOpenGraphData']
+        );
     }
 
     public function addSiteSettingsFilters($event)
     {
         $inputFilter = $event->getParam('inputFilter');
         $inputFilter->get('sharing')->add([
-                    'name' => 'sharing_methods',
-                    'required' => false,
-                ]);
+            'name' => 'sharing_methods',
+            'required' => false,
+        ]);
     }
 
     public function addSiteEnableCheckbox($event)
@@ -99,7 +117,6 @@ class Module extends AbstractModule
         $fieldset->setLabel('Sharing'); // @translate
 
         $enabledMethods = $siteSettings->get('sharing_methods', []);
-        $placement = $siteSettings->get('sharing_placement', 'view.show.before');
         $fieldset->add([
             'name' => 'sharing_methods',
             'type' => 'multiCheckbox',
@@ -107,35 +124,35 @@ class Module extends AbstractModule
                 'label' => 'Enable Sharing module for these methods', // @translate
                 'value_options' => [
                     'fb' => [
-                                    'label' => 'Facebook', // @translate
-                                    'value' => 'fb',
-                                    'selected' => in_array('fb', $enabledMethods),
-                                    ],
+                        'label' => 'Facebook', // @translate
+                        'value' => 'fb',
+                        'selected' => in_array('fb', $enabledMethods),
+                    ],
                     'twitter' => [
-                                    'label' => 'Twitter', // @translate
-                                    'value' => 'twitter',
-                                    'selected' => in_array('twitter', $enabledMethods),
-                                   ],
+                        'label' => 'Twitter', // @translate
+                        'value' => 'twitter',
+                        'selected' => in_array('twitter', $enabledMethods),
+                    ],
                     'tumblr' => [
-                                    'label' => 'Tumblr', // @translate
-                                    'value' => 'tumblr',
-                                    'selected' => in_array('tumblr', $enabledMethods),
-                                   ],
+                        'label' => 'Tumblr', // @translate
+                        'value' => 'tumblr',
+                        'selected' => in_array('tumblr', $enabledMethods),
+                    ],
                     'pinterest' => [
-                                    'label' => 'Pinterest', // @translate
-                                    'value' => 'pinterest',
-                                    'selected' => in_array('pinterest', $enabledMethods),
-                                   ],
+                        'label' => 'Pinterest', // @translate
+                        'value' => 'pinterest',
+                        'selected' => in_array('pinterest', $enabledMethods),
+                    ],
                     'email' => [
-                                    'label' => 'Email', // @translate
-                                    'value' => 'email',
-                                    'selected' => in_array('email', $enabledMethods),
-                                   ],
+                        'label' => 'Email', // @translate
+                        'value' => 'email',
+                        'selected' => in_array('email', $enabledMethods),
+                    ],
                     'embed' => [
-                                    'label' => 'Embed codes', // @translate
-                                    'value' => 'embed',
-                                    'selected' => in_array('embed', $enabledMethods),
-                                   ],
+                        'label' => 'Embed codes', // @translate
+                        'value' => 'embed',
+                        'selected' => in_array('embed', $enabledMethods),
+                    ],
                 ],
             ],
             'attributes' => [
@@ -144,10 +161,10 @@ class Module extends AbstractModule
         ]);
 
         $fieldset->add([
-            'name' => 'sharing_placement',
+            'name' => 'sharing_placement_show',
             'type' => 'radio',
             'options' => [
-                'label' => "Sharing buttons placement on the page", // @translate
+                'label' => "Sharing buttons placement on show page", // @translate
                 'value_options' => [
                     'top' => [
                         'label' => 'Top', // @translate
@@ -158,14 +175,83 @@ class Module extends AbstractModule
                         'label' => 'Bottom', //@translate
                         'value' => 'view.show.after',
                     ],
-
+                    'none' => [
+                        'label' => 'None', //@translate
+                        'value' => 'none',
+                    ],
                 ],
             ],
             'attributes' => [
                 'required' => false,
-                'value' => $placement,
+                'value' => $siteSettings->get('sharing_placement_show', 'view.show.before'),
             ],
         ]);
+
+        $fieldset->add([
+            'name' => 'sharing_placement_browse',
+            'type' => 'radio',
+            'options' => [
+                'label' => "Sharing buttons placement on browse page", // @translate
+                'value_options' => [
+                    'top' => [
+                        'label' => 'Top', // @translate
+                        'value' => 'view.browse.before',
+                    ],
+                    'bottom' => [
+                        'label' => 'Bottom', //@translate
+                        'value' => 'view.browse.after',
+                    ],
+                    'none' => [
+                        'label' => 'None', //@translate
+                        'value' => 'none',
+                    ],
+                ],
+            ],
+            'attributes' => [
+                'required' => false,
+                'value' => $siteSettings->get('sharing_placement_browse', 'view.browse.before'),
+            ],
+        ]);
+
+        $fieldset->add([
+            'name' => 'sharing_facebook_locale',
+            'type' => 'Text',
+            'options' => [
+                'label' => 'Facebook Locale', // @translate
+            ],
+            'attributes' => [
+                'required' => false,
+                'value' => $siteSettings->get('sharing_facebook_locale', 'en_US'),
+            ],
+        ]);
+
+        $fieldset->add([
+            'name' => 'sharing_facebook_app_id',
+            'type' => 'Text',
+            'options' => [
+                'label' => 'Facebook App Id', // @translate
+                'info' => 'See https://developers.facebook.com/apps/', // @translate
+            ],
+            'attributes' => [
+                'required' => false,
+                'value' => $siteSettings->get('sharing_facebook_app_id', ''),
+            ],
+        ]);
+
+        $fieldset->add([
+            'type' => 'checkbox',
+            'name' => 'sharing_facebook_collect_analytics',
+            'options' => [
+                'label' => 'Facebook collect Analytics', // @translate
+                'info' => 'Do you want your Facebook app to collect analytics on sharing button ?', // @translate
+                'checked_value' => 'yes',
+                'unchecked_value' => 'no',
+            ],
+            'attributes' => [
+                'value' => $siteSettings->get('sharing_facebook_collect_analytics', 'no'),
+            ],
+        ]);
+
         $form->add($fieldset);
     }
 
@@ -177,30 +263,39 @@ class Module extends AbstractModule
         $escape = $view->plugin('escapeHtml');
         $description = false;
         $image = false;
+
         switch ($controller) {
-                case 'Omeka\Controller\Site\Item':
+            case 'Omeka\Controller\Site\Item':
+                if (isset($view->itemSet)) {
+                    $description = $escape($view->itemSet->displayDescription());
+                    $title = $escape($view->itemSet->displayTitle()) . ' · ' . $view->setting('installation_title', 'Omeka S');
+                    if ($thumbnail = $view->itemSet->thumbnail()) {
+                        $image = $escape($thumbnail->assetUrl());
+                    }
+                } else if (isset($view->item)) {
                     $description = $escape($view->item->displayDescription());
-                    $title = $escape($view->item->displayTitle()).' · '.$view->setting('installation_title', 'Omeka S');
+                    $title = $escape($view->item->displayTitle()) . ' · ' . $view->setting('installation_title', 'Omeka S');
                     if ($primaryMedia = $view->item->primaryMedia()) {
                         $image = $escape($primaryMedia->thumbnailUrl('large'));
                     }
+                }
                 break;
 
-                case 'Omeka\Controller\Site\Page':
-                    $blocks = $view->page->blocks();
-                    $title = $view->headTitle()->renderTitle().' · '.$view->setting('installation_title', 'Omeka S');
-                    foreach ($blocks as $block) {
-                        $attachments = $block->attachments();
-                        foreach ($attachments as $attachment) {
-                            $item = $attachment->item();
-                            if ($primaryMedia = $item->primaryMedia()) {
-                                $image = $escape($primaryMedia->thumbnailUrl('large'));
-                                break 2;
-                            }
+            case 'Omeka\Controller\Site\Page':
+                $blocks = $view->page->blocks();
+                $title = $view->headTitle()->renderTitle() . ' · ' . $view->setting('installation_title', 'Omeka S');
+                foreach ($blocks as $block) {
+                    $attachments = $block->attachments();
+                    foreach ($attachments as $attachment) {
+                        $item = $attachment->item();
+                        if ($primaryMedia = $item->primaryMedia()) {
+                            $image = $escape($primaryMedia->thumbnailUrl('large'));
+                            break 2;
                         }
                     }
+                }
                 break;
-            }
+        }
         $view->headTitle()->setSeparator(' · ');
         $view->headMeta()->appendProperty('og:title', $title);
         $view->headMeta()->appendProperty('og:type', 'website');
@@ -214,11 +309,24 @@ class Module extends AbstractModule
         }
     }
 
+    public function viewBrowse($event)
+    {
+        $siteSettings = $this->getServiceLocator()->get('Omeka\Settings\Site');
+        $placement = $siteSettings->get('sharing_placement_browse', 'view.browse.before');
+        $this->displayShareOptions($event, $placement);
+    }
+
     public function viewShow($event)
     {
         $siteSettings = $this->getServiceLocator()->get('Omeka\Settings\Site');
+        $placement = $siteSettings->get('sharing_placement_show', 'view.show.before');
+        $this->displayShareOptions($event, $placement);
+    }
+
+    function displayShareOptions($event, $placement)
+    {
+        $siteSettings = $this->getServiceLocator()->get('Omeka\Settings\Site');
         $enabledMethods = $siteSettings->get('sharing_methods');
-        $placement = $siteSettings->get('sharing_placement', 'view.show.before');
         $eventName = $event->getName();
         if (!empty($enabledMethods) && ($eventName == $placement)) {
             $view = $event->getTarget();
@@ -227,37 +335,27 @@ class Module extends AbstractModule
             $view->headLink()->appendStylesheet($view->assetUrl('css/sharing.css', 'Sharing'));
             $escape = $view->plugin('escapeHtml');
             $translator = $this->getServiceLocator()->get('MvcTranslator');
-            $siteSlug = $this->getServiceLocator()->get('Application')
-                ->getMvcEvent()->getRouteMatch()->getParam('site-slug');
+            $siteSlug = $this->getServiceLocator()->get('Application')->getMvcEvent()->getRouteMatch()->getParam('site-slug');
 
             echo $view->partial('share-buttons',
-                    ['escape' => $escape,
-                          'translator' => $translator,
-                          'enabledMethods' => $enabledMethods,
-                          'itemId' => isset($view->item) ? $view->item->id() : false,
-                          'pageId' => isset($view->page) ? $view->page->id() : false,
-                          'siteSlug' => $siteSlug,
-                            ]
-                    );
+                ['escape' => $escape,
+                    'translator' => $translator,
+                    'enabledMethods' => $enabledMethods,
+                    'itemId' => isset($view->item) ? $view->item->id() : false,
+                    'pageId' => isset($view->page) ? $view->page->id() : false,
+                    'siteSlug' => $siteSlug,
+                ]
+            );
 
-            $fbJavascript = "
-            <script>
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml      : true,
-                  version    : 'v2.5'
-                });
-              };
+            $fbLocale = $siteSettings->get('sharing_facebook_locale', 'en_US');
+            $fbAnalytics = $siteSettings->get('sharing_facebook_collect_analytics', 'no') === 'yes' ? '&autoLogAppEvents=1' : '';
+            $fbAppId = $siteSettings->get('sharing_facebook_app_id', null) ? '&appId=' . $siteSettings->get('sharing_facebook_app_id') : '';
 
-              (function(d, s, id){
-                 var js, fjs = d.getElementsByTagName(s)[0];
-                 if (d.getElementById(id)) {return;}
-                 js = d.createElement(s); js.id = id;
-                 js.src = '//connect.facebook.net/en_US/sdk.js';
-                 fjs.parentNode.insertBefore(js, fjs);
-               }(document, 'script', 'facebook-jssdk'));
+            $fbJavascript = '
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/' . $fbLocale . '/sdk.js#xfbml=1&version=v7.0' . $fbAnalytics . $fbAppId . '">
             </script>
-            ";
+            ';
 
             $pinterestJavascript = '
                 <script
@@ -272,7 +370,7 @@ class Module extends AbstractModule
             ';
 
             foreach ($enabledMethods as $method) {
-                $js = $method.'Javascript';
+                $js = $method . 'Javascript';
                 if (isset($$js)) {
                     echo $$js;
                 }
